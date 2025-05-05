c196182435153ea374ca3e1ec7abfaafd7677f2a
Theanh130124 <theanhtran13012004@gmail.com>
2025-05-05 23:09:24 +0700

    Update

---
+++ SpringBootApp/src/main/java/com/theanh1301/SpringBootApp/HelloSpringController.java
@@ -0,0 +1,18 @@
+package com.theanh1301.SpringBootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloSpringController {
+
+
+
+
+
+        @GetMapping("/hello")
+        String sayHello(){
+            return "Hello World!";
+        }
+
+
+}
