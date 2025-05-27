546dc9ca43aa571173c4e91eb00202db02e76a60
Theanh130124 <theanhtran13012004@gmail.com>
2025-05-27 20:44:07 +0700

    Update Authorization by JWTDecoder

--- src/main/java/com/theanh1301/myapp/config/SecurityConfig.java
+++ src/main/java/com/theanh1301/myapp/config/SecurityConfig.java
@@ -1,25 +1,51 @@
 package com.theanh1301.myapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.JwkSetUriJwtDecoderBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+
 
 @Configuration
 @EnableWebSecurity // no dc enable san roi -> khong bat cung duoc
 public class SecurityConfig {
 
-    private final String [] PUBLIC_ENDPOINTS = {"/api/users", "/api/auth/login" ,"/api/auth/introspect"};
+    private final String[] PUBLIC_ENDPOINTS = {"/api/users", "/api/auth/login", "/api/auth/introspect"};
+
+    @Value("${jwt.signerKey}")
+    private String jwtSignerKey;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.
-                authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).
+                authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).
                         permitAll().anyRequest().authenticated());
+        //cung cap token hop le vao header(cai nay xu ly ktra)
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
+
+
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKey = new SecretKeySpec(jwtSignerKey.getBytes(), "HS512");
+        return  NimbusJwtDecoder.withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS512).build();
+    }
+
 }
+
+
